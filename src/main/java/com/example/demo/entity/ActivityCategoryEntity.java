16f5a048b4d8721f8fe9d1192e457e8273951cde
bala-amypo <bala@amypo.in>
2025-12-18 06:40:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/ActivityCategoryEntity.java
+++ src/main/java/com/example/demo/entity/ActivityCategoryEntity.java
@@ -1,26 +1,63 @@
-
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
-public class ActivityCategory(){
-    private long id;
+public class ActivityCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String categoryName;
     private String description;
     private LocalDateTime createdAt;
 
-}
+    public ActivityCategory(Long id, String categoryName, String description, LocalDateTime createdAt) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
 
-public ActivityCategory(){
+    public ActivityCategory() {
+       
+    }
 
-}
+    public Long getId() {
+        return this.id;
+    }
 
-public ActivityCategory(long id,String categoryName,String description, LocalDateTime createdAt){
-    this.categoryName=categoryName;
-    this.description=description;
-    this.createdAt=createdAt;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
 
 
