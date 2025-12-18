413abf9511b1e3e95df248f4a3cc6a2112a315bc
bala-amypo <bala@amypo.in>
2025-12-18 06:28:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ActivityCategoryEntity.java
@@ -0,0 +1,17 @@
+
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class ActivityCategory(){
+    private long id;
+    private String categoryName;
+    private String description;
+    private LocalDateTime createdAt;
+
+}
+
+public A
+
