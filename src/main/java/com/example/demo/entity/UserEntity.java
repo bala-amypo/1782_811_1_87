10ed495e10d8404f1cfbd0c32bacfe4cb3855b75
bala-amypo <bala@amypo.in>
2025-12-18 08:22:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -13,16 +13,11 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String fullName;
-
     @Column(unique = true, nullable = false)
     private String email;
-
     private String role;  
-
     private LocalDateTime createdAt;
-
     public UserEntity() {
         this.createdAt = LocalDateTime.now();
     }
@@ -35,35 +30,27 @@ public class UserEntity {
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getFullName() {
         return fullName;
     }
-
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getRole() {
         return role;
     }
-
     public void setRole(String role) {
         this.role = role;
     }
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
