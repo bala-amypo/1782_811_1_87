8b1c3e94d730409ee5f164c36d8fbbbb87833647
bala-amypo <bala@amypo.in>
2025-12-20 13:47:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ActivityTypeController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.ActivityTypeEntity;
+import com.example.demo.service.ActivityTypeService;
+
+@RestController
+@RequestMapping("/api/types")
+public class ActivityTypeController {
+
+    private final ActivityTypeService service;
+
+    public ActivityTypeController(ActivityTypeService service) {
+        this.service = service;
+    }
+
+    // POST /api/types/category/{categoryId}
+    @PostMapping("/category/{categoryId}")
+    public ActivityTypeEntity createType(
+            @PathVariable Long categoryId,
+            @RequestBody ActivityTypeEntity type) {
+        return service.createType(categoryId, type);
+    }
+
+    // GET /api/types/category/{categoryId}
+    @GetMapping("/category/{categoryId}")
+    public List<ActivityTypeEntity> getTypesByCategory(
+            @PathVariable Long categoryId) {
+        return service.getTypesByCategory(categoryId);
+    }
+
+    // GET /api/types/{id}
+    @GetMapping("/{id}")
+    public ActivityTypeEntity getType(@PathVariable Long id) {
+        return service.getType(id);
+    }
+}
