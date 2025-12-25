957973681cac0c5a1bf9e2798e5a9d89e1bc86df
bala-amypo <bala@amypo.in>
2025-12-25 12:44:56 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +17,9 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("User Management API")
                         .description("REST API for user registration, login and management")
-                        .version("1.0"));
+                        .version("1.0"))
+                .servers(List.of(
+                        new Server().url("https://9376.pro604cr.amypo.ai/").description("Server")
+                ));
     }
 }
