889e71058103fa6a7b58b8e930ee1d871215208b
bala-amypo <bala@amypo.in>
2025-12-29 08:16:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActivityLogRepository.java
+++ src/main/java/com/example/demo/repository/ActivityLogRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.ActivityCategory;
+import com.example.demo.entity.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface ActivityCategoryRepository extends JpaRepository<ActivityCategory, Long> {
-    boolean existsByCategoryName(String categoryName);
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
+    List<ActivityLog> findByUser_IdAndActivityDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
+    List<ActivityLog> findByUser_Id(Long userId);
 }
